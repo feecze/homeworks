7e0e3f18ed21c6ef1bbcf473b5680e2c67f2b9f0
paxarpp <pavelpp1974@gmail.com>
2019-04-07 23:04:57 +0300

    Переделал на класс и добавил стилей

--- basket/HW9.js
+++ basket/HW9.js
@@ -1,49 +1,66 @@
-let products = [
-    {
-        name: 'avocado',
-        price: 100,
-        description: 'tasty and healthy'
-    },
-    {
-        name: 'chips',
-        price: 25,
-        description: 'tasty and unhealthy'
-    },
-    {
-        name: 'water',
-        price: 10,
-        description: 'tasty and very healthy'
-    },
-    {
-        name: 'cola',
-        price: 40,
-        description: 'tasty and very unhealthy'
-    },
-];
+class Shop {
+    products = [
+        {
+            name: 'avocado',
+            price: 100,
+            description: 'tasty and healthy'
+        },
+        {
+            name: 'chips',
+            price: 25,
+            description: 'tasty and unhealthy'
+        },
+        {
+            name: 'water',
+            price: 10,
+            description: 'tasty and very healthy'
+        },
+        {
+            name: 'cola',
+            price: 40,
+            description: 'tasty and very unhealthy'
+        },
+    ];
 
-let basket = [];
+    basket = [];
 
-function renderProducts(products) {
-    let list = document.querySelector('.container .products_list');
-    for (i = 0; i < products.length; i++) {
-        let li = document.createElement('li');
-        li.classList.add('product_card');
-        
-        let name = document.createElement('p');
-        name.textContent = products[i].name;
-        li.appendChild(name);
+    renderProducts() {
+        let list = document.querySelector('.container .products_list');
+        for (let i = 0; i < this.products.length; i++) {
+            let li = document.createElement('li');
+            li.classList.add('product_card');
+            
+            let name = document.createElement('p');
+            name.textContent = this.products[i].name;
+            li.appendChild(name);
+    
+            let price = document.createElement('p');
+            price.textContent = this.products[i].price;
+            li.appendChild(price);
+    
+            let description = document.createElement('p');
+            description.textContent = this.products[i].description;
+            li.appendChild(description);
+    
+            list.appendChild(li);
+        }    
+    };
 
-        let price = document.createElement('p');
-        price.textContent = products[i].price;
-        li.appendChild(price);
-
-        let description = document.createElement('p');
-        description.textContent = products[i].description;
-        li.appendChild(description);
-
-        list.appendChild(li);
+    addProducts(product) {
+        if(product.name) {
+            this.products.push(product);
+        }
     }
+}
 
+let shop = new Shop();
+
+
+let product1 = {
+    name: 'socks',
+    price: 440,
+    description: 'clothes'
 };
 
-renderProducts(products)
+shop.addProducts(product1)
+shop.renderProducts();
