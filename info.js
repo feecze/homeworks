5a0aad48213ecff4ac574e6a3fa00e50e8061bee
paxarpp <pavelpp1974@gmail.com>
2019-08-15 22:54:49 +0300

    Тупа веселился

---
+++ info.js
@@ -0,0 +1,26 @@
+let Admin = {
+  name: {
+    firstName: 'Nikita',
+    lastName: 'Adminov'
+  },
+
+  auth: {
+    login: 'nikitaadminov666',
+    nickname: 'adskiyNIKITOS',
+    password: 'ochen_slosniy_parol'
+  },
+
+  adress: {
+    home: {
+      city: 'SECRETINFO',
+      street: 'SECRETINFO',
+      metro: 'SECRETINFO'
+    },
+
+    work: {
+      city: 'Moscow',
+      street: 'Nagorniy Proezd',
+      metro: 'Verhnie Kotli'
+    }
+  }
+};
